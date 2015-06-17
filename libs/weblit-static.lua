d929634cd13bbe6be6dfa9b5a0ed3c17d0438294
Tim Caswell <tim@creationix.com>
2015-06-17 12:48:55 -0500

    Fix metadata on weblit-static

--- libs/weblit-static.lua
+++ libs/weblit-static.lua
@@ -1,11 +1,11 @@
 exports.name = "creationix/weblit-static"
-exports.version = "0.3.3"
+exports.version = "0.3.3-1"
 exports.dependencies = {
   "creationix/mime@0.1.2",
   "creationix/hybrid-fs@0.1.1",
 }
-exports.description = "The auto-headers middleware helps Weblit apps implement proper HTTP semantics"
-exports.tags = {"weblit", "middleware", "http"}
+exports.description = "A weblit middleware for serving static files from disk or bundle."
+exports.tags = {"weblit", "middleware", "static"}
 exports.license = "MIT"
 exports.author = { name = "Tim Caswell" }
 exports.homepage = "https://github.com/creationix/weblit/blob/master/libs/weblit-auto-headers.lua"
