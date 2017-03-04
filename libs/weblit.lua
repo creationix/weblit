e9f87ca8e40e1f2002f33bf3007be0e07f16ded2
Tim Caswell <tim@creationix.com>
2017-03-03 23:33:54 -0600

    Move weblit metapackage to single-file format

---
+++ libs/weblit.lua
@@ -0,0 +1,31 @@
+--[[lit-meta
+  name = "creationix/weblit"
+  version = "3.0.1"
+  dependencies = {
+    "creationix/weblit-app@3.0.0",
+    "creationix/weblit-auto-headers@2.0.2",
+    "creationix/weblit-etag-cache@2.0.0",
+    "creationix/weblit-logger@02.0.0",
+    "creationix/weblit-cors@2.0.0",
+    "creationix/weblit-static@2.1.0",
+    "creationix/weblit-websocket@3.0.0",
+  }
+  files = {
+    "package.lua",
+    "init.lua",
+  }
+  description = "This Weblit metapackage brings in all the official Weblit modules."
+  tags = {"weblit", "meta"}
+  license = "MIT"
+  author = { name = "Tim Caswell" }
+  homepage = "https://github.com/creationix/weblit"
+--]]
+
+return {
+  app = require('weblit-app'),
+  autoHeaders = require('weblit-auto-headers'),
+  etagCache = require('weblit-etag-cache'),
+  logger = require('weblit-logger'),
+  static = require('weblit-static'),
+  websocket = require('weblit-websocket'),
+}
